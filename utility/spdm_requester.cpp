2f131af9dcdd6c23985a23e9896fd34edb2e5fc9
Eddyhan <eddyhan@ami.com>
2022-07-11 11:20:08 +0800

    Remove sleep() in SPDM requester unit test program.

--- utility/spdm_requester.cpp
+++ utility/spdm_requester.cpp
@@ -18,21 +18,66 @@
 #include "spdmtransport_mctp.hpp"
 
 #include <CLI/CLI.hpp>
-#include <boost/asio.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/asio/signal_set.hpp>
 #include <boost/asio/steady_timer.hpp>
+#include <phosphor-logging/log.hpp>
+#include <sdbusplus/asio/object_server.hpp>
 
 #include <iostream>
+#include <unordered_set>
 
 extern spdmapplib::SpdmConfiguration getConfigurationFromEntityManager(
     std::shared_ptr<sdbusplus::asio::connection> conn,
     const std::string& configurationName);
+
+static std::shared_ptr<boost::asio::io_context> ioc =
+    std::make_shared<boost::asio::io_context>();
+static std::shared_ptr<sdbusplus::asio::connection> conn =
+    std::make_shared<sdbusplus::asio::connection>(*ioc);
+static auto pSpdmRequester = spdmapplib::createRequester();
+static auto trans = std::make_shared<spdmtransport::spdmTransportMCTP>(
+    spdmtransport::TransportIdentifier::mctpOverSMBus);
+static spdmapplib::SpdmConfiguration spdmRequesterCfg{};
+static boost::asio::steady_timer requesterTimer(*ioc);
+static uint8_t responderEid;
+
+using ConfigurationField =
+    std::variant<bool, uint64_t, std::string, std::vector<uint64_t>>;
+
+using ConfigurationMap = std::unordered_map<std::string, ConfigurationField>;
+
+static const std::string spdmTypeName =
+    "xyz.openbmc_project.Configuration.SPDMConfiguration";
+
+static std::unordered_set<std::string> startedUnits;
+
+/**
+ * @brief Function to get SPDM configuration path in EntityManager.
+ *
+ */
+static std::vector<std::string> getConfigurationPaths()
+{
+    auto method_call = conn->new_method_call(
+        "xyz.openbmc_project.ObjectMapper",
+        "/xyz/openbmc_project/object_mapper",
+        "xyz.openbmc_project.ObjectMapper", "GetSubTreePaths");
+
+    method_call.append("/xyz/openbmc_project/inventory/system/board", 2,
+                       std::array<std::string, 1>({spdmTypeName}));
+
+    auto reply = conn->call(method_call);
+    std::vector<std::string> paths;
+    reply.read(paths);
+    return paths;
+}
 
 /**
  * @brief Function to display Unit test menu.
  *
  */
 
-void displayMenu(void)
+static void displayMenu(void)
 {
     std::cerr << "1: doAuthentication()" << std::endl;
     std::cerr << "2: doMeasurement()" << std::endl;
@@ -46,7 +91,7 @@ void displayMenu(void)
  *
  * @param vec : The vector to be dumped
  */
-void dumpVector(std::vector<unsigned char> vec)
+static void dumpVector(std::vector<unsigned char> vec)
 {
     std::cerr << __func__ << " size: " << vec.size() << std::hex << std::endl;
     for (unsigned int i = 0; i < vec.size(); ++i)
@@ -59,49 +104,25 @@ void dumpVector(std::vector<unsigned char> vec)
     }
     std::cerr << std::dec << std::endl;
 }
-int main(int argc, char* argv[])
+
+/**
+ * @brief Main function of SPDM requester unit test.
+ *
+ */
+static void startSPDMRequester()
 {
-    auto pspdmRequester = spdmapplib::createRequester();
     spdmtransport::transportEndPoint responderCfg = {
         spdmtransport::TransportIdentifier::mctpOverSMBus, 0};
-    auto ioc = std::make_shared<boost::asio::io_context>();
-    auto conn = std::make_shared<sdbusplus::asio::connection>(*ioc);
-    auto trans = std::make_shared<spdmtransport::spdmTransportMCTP>(
-        spdmtransport::TransportIdentifier::mctpOverSMBus);
-    boost::asio::steady_timer timer(*ioc);
-    spdmapplib::SpdmConfiguration spdmRequesterCfg{};
-    uint8_t eid;
-    CLI::App app("SPDM requester verify tool");
-    app.add_option("--eid", eid, "Responder MCTP EID    : uint8_t")->required();
-    CLI11_PARSE(app, argc, argv);
 
-    std::cerr << "Assigned responder EID: " << static_cast<uint16_t>(eid)
-              << std::endl;
-    responderCfg.devIdentifier = eid;
-    do
-    {
-        spdmRequesterCfg =
-            getConfigurationFromEntityManager(conn, "SPDM_requester");
-        if (spdmRequesterCfg.version)
-        {
-            break;
-        }
-        else
-        {
-            std::cerr
-                << "Can't get SPDM requester configuration from EntityManager. Wait for 1 second."
-                << std::endl;
-            sleep(1);
-        }
-    } while (true);
+    phosphor::logging::log<phosphor::logging::level::INFO>(
+        "Staring SPDM requester!!");
+    responderCfg.devIdentifier = responderEid;
 
-    if (pspdmRequester->initRequester(ioc, conn, trans, responderCfg,
+    if (pSpdmRequester->initRequester(ioc, conn, trans, responderCfg,
                                       spdmRequesterCfg) == 0)
     {
-        std::cerr << "spdm_requester started." << std::endl;
-        int selTest = 0;
-        timer.expires_after(std::chrono::seconds(1));
-        timer.async_wait([&](boost::system::error_code ec) {
+        requesterTimer.expires_after(std::chrono::seconds(1));
+        requesterTimer.async_wait([&](boost::system::error_code ec) {
             if (ec == boost::asio::error::operation_aborted)
             {
                 return;
@@ -111,6 +132,7 @@ int main(int argc, char* argv[])
                 std::cerr << "Timer error " << ec.message() << std::endl;
                 return;
             }
+            int selTest = 0;
             bool testRun = true;
             while (testRun)
             {
@@ -122,32 +144,158 @@ int main(int argc, char* argv[])
                 {
                     case 1:
                         std::cerr << "Execute doAuthentication()." << std::endl;
-                        pspdmRequester->doAuthentication();
+                        pSpdmRequester->doAuthentication();
                         break;
                     case 2:
                         std::cerr << "Execute doMeasurement()." << std::endl;
-                        pspdmRequester->doMeasurement(NULL);
+                        pSpdmRequester->doMeasurement(nullptr);
                         break;
                     case 3:
                         std::cerr << "Execute getCertificate()." << std::endl;
-                        dumpVector(pspdmRequester->getCertificate().value());
+                        dumpVector(pSpdmRequester->getCertificate().value());
                         break;
                     case 4:
                         std::cerr << "Execute getMeasurements()." << std::endl;
-                        dumpVector(pspdmRequester->getMeasurements().value());
+                        dumpVector(pSpdmRequester->getMeasurements().value());
                         break;
                     default:
                         testRun = false;
                         std::cerr << "Quit!" << std::endl;
+                        ioc->stop();
                 }
             }
-            ioc->stop();
         });
-        ioc->run();
     }
     else
     {
         std::cerr << "spdm_requester init failed." << std::endl;
     }
+}
+
+/**
+ * @brief Function to start SPDM requester function test.
+ *
+ * @param spdmConfig : The configuration name to in entitymanager.
+ */
+
+static void startExistingConfigurations(std::string& spdmConfig)
+{
+    std::vector<std::string> configurationPaths;
+    try
+    {
+        configurationPaths = getConfigurationPaths();
+    }
+    catch (const std::exception& e)
+    {
+        phosphor::logging::log<phosphor::logging::level::WARNING>(
+            (std::string("Could not retrieve existing configurations: ") +
+             e.what())
+                .c_str());
+        return;
+    }
+
+    for (const auto& objectPath : configurationPaths)
+    {
+        if (startedUnits.count(objectPath) != 0)
+        {
+            continue;
+        }
+        phosphor::logging::log<phosphor::logging::level::DEBUG>(
+            (std::string("Found config: ") + objectPath).c_str());
+
+        if (objectPath.find(spdmConfig) != objectPath.npos)
+        {
+            phosphor::logging::log<phosphor::logging::level::DEBUG>(
+                ("Matched : " + spdmConfig + "! Reading configuration from" +
+                 objectPath)
+                    .c_str());
+            spdmRequesterCfg =
+                getConfigurationFromEntityManager(conn, spdmConfig);
+            startSPDMRequester();
+        }
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    std::string requesterConfigName{"SPDM_requester"};
+
+    CLI::App app("SPDM requester verify tool");
+    app.add_option("--eid", responderEid, "Responder MCTP EID    : uint8_t")
+        ->required();
+    CLI11_PARSE(app, argc, argv);
+
+    std::cerr << "Assigned responder EID: "
+              << static_cast<uint16_t>(responderEid) << std::endl;
+
+    startExistingConfigurations(requesterConfigName);
+
+    if (spdmRequesterCfg.version)
+    {
+        phosphor::logging::log<phosphor::logging::level::INFO>(
+            "SPDM requester started.");
+    }
+    else
+    {
+        phosphor::logging::log<phosphor::logging::level::INFO>(
+            "SPDM requester configuration not found!!");
+    }
+    std::vector<std::string> units;
+
+    namespace rules = sdbusplus::bus::match::rules;
+
+    auto match = std::make_unique<sdbusplus::bus::match::match>(
+        *conn,
+        rules::interfacesAdded() + rules::path_namespace("/") +
+            rules::sender("xyz.openbmc_project.EntityManager"),
+        [&units, &requesterConfigName](sdbusplus::message::message& message) {
+            if (message.is_method_error())
+            {
+                phosphor::logging::log<phosphor::logging::level::ERR>(
+                    "Callback method error");
+                return;
+            }
+            sdbusplus::message::object_path unitPath;
+            std::unordered_map<std::string, ConfigurationMap> interfacesAdded;
+            try
+            {
+                message.read(unitPath, interfacesAdded);
+            }
+            catch (const std::exception& e)
+            {
+                phosphor::logging::log<phosphor::logging::level::ERR>(
+                    "Message read error");
+                return;
+            }
+
+            if (startedUnits.count(unitPath) != 0)
+            {
+                return;
+            }
+            for (const auto& interface : interfacesAdded)
+            {
+                if (interface.first != spdmTypeName)
+                {
+                    continue;
+                }
+                std::cerr << "Config found in match rule!" << std::endl;
+                if (spdmRequesterCfg.version)
+                {
+                    std::cerr << "spdm requester had started before."
+                              << std::endl;
+                }
+                else
+                {
+                    std::cerr << "spdm requester starting..." << std::endl;
+                    startExistingConfigurations(requesterConfigName);
+                }
+            }
+        });
+
+    boost::asio::signal_set signals(*ioc, SIGINT, SIGTERM);
+    signals.async_wait(
+        [](const boost::system::error_code&, const int&) { ioc->stop(); });
+
+    ioc->run();
     return 0;
 }
