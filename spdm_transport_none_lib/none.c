89c6281652141c1513c18d43b902db4b9aef1e08
Prithvi A Pai <prithvi.a.pai@intel.com>
2022-10-15 06:06:36 +0000

    removed clang

--- spdm_transport_none_lib/none.c
+++ spdm_transport_none_lib/none.c
@@ -1,8 +1,7 @@
 /**
     Copyright Notice:
     Copyright 2021 DMTF. All rights reserved.
-    License: BSD 3-Clause License. For full text see link:
-https://github.com/DMTF/libspdm/blob/main/LICENSE.md
+    License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/libspdm/blob/main/LICENSE.md
 **/
 
 #include "library/spdm_transport_none_lib.h"
@@ -10,30 +9,25 @@ https://github.com/DMTF/libspdm/blob/main/LICENSE.md
 /**
   Encode a normal message or secured message to a transport message.
 
-  @param  session_id                    Indicates if it is a secured message
-protected via SPDM session. If session_id is NULL, it is a normal message. If
-session_id is NOT NULL, it is a secured message.
-  @param  message_size                  size in bytes of the message data
-buffer.
-  @param  message                      A pointer to a source buffer to store the
-message.
-  @param  transport_message_size         size in bytes of the transport message
-data buffer.
-  @param  transport_message             A pointer to a destination buffer to
-store the transport message.
+  @param  session_id                    Indicates if it is a secured message protected via SPDM session.
+                                       If session_id is NULL, it is a normal message.
+                                       If session_id is NOT NULL, it is a secured message.
+  @param  message_size                  size in bytes of the message data buffer.
+  @param  message                      A pointer to a source buffer to store the message.
+  @param  transport_message_size         size in bytes of the transport message data buffer.
+  @param  transport_message             A pointer to a destination buffer to store the transport message.
 
   @retval RETURN_SUCCESS               The message is encoded successfully.
-  @retval RETURN_INVALID_PARAMETER     The message is NULL or the message_size
-is zero.
+  @retval RETURN_INVALID_PARAMETER     The message is NULL or the message_size is zero.
 **/
-return_status none_encode_message(const uint32_t* session_id,
-                                  uintn message_size, const void* message,
-                                  uintn* transport_message_size,
-                                  void* transport_message)
+return_status none_encode_message(const uint32_t *session_id, uintn message_size,
+                  const void *message,
+                  uintn *transport_message_size,
+                  void *transport_message)
 {
     *transport_message_size = message_size;
-    copy_mem((uint8_t*)transport_message, message, message_size);
-    zero_mem((uint8_t*)transport_message + message_size,
+    copy_mem((uint8_t *)transport_message, message, message_size);
+    zero_mem((uint8_t *)transport_message + message_size,
              *transport_message_size - message_size);
     return RETURN_SUCCESS;
 }
@@ -41,25 +35,20 @@ return_status none_encode_message(const uint32_t* session_id,
 /**
   Decode a transport message to a normal message or secured message.
 
-  @param  session_id                    Indicates if it is a secured message
-protected via SPDM session. If *session_id is NULL, it is a normal message. If
-*session_id is NOT NULL, it is a secured message.
-  @param  transport_message_size         size in bytes of the transport message
-data buffer.
-  @param  transport_message             A pointer to a source buffer to store
-the transport message.
-  @param  message_size                  size in bytes of the message data
-buffer.
-  @param  message                      A pointer to a destination buffer to
-store the message.
+  @param  session_id                    Indicates if it is a secured message protected via SPDM session.
+                                       If *session_id is NULL, it is a normal message.
+                                       If *session_id is NOT NULL, it is a secured message.
+  @param  transport_message_size         size in bytes of the transport message data buffer.
+  @param  transport_message             A pointer to a source buffer to store the transport message.
+  @param  message_size                  size in bytes of the message data buffer.
+  @param  message                      A pointer to a destination buffer to store the message.
   @retval RETURN_SUCCESS               The message is encoded successfully.
-  @retval RETURN_INVALID_PARAMETER     The message is NULL or the message_size
-is zero.
+  @retval RETURN_INVALID_PARAMETER     The message is NULL or the message_size is zero.
 **/
-return_status none_decode_message(uint32_t** session_id,
-                                  uintn transport_message_size,
-                                  const void* transport_message,
-                                  uintn* message_size, void* message)
+return_status none_decode_message(uint32_t **session_id,
+                  uintn transport_message_size,
+                  const void *transport_message,
+                  uintn *message_size, void *message)
 {
     *message_size = transport_message_size;
     copy_mem(message, transport_message, transport_message_size);
@@ -71,17 +60,16 @@ return_status none_decode_message(uint32_t** session_id,
 
   This value is transport layer specific.
 
-  @param sequence_number        The current sequence number used to encode or
-decode message.
-  @param sequence_number_buffer  A buffer to hold the sequence number output
-used in the secured message. The size in byte of the output buffer shall be 8.
+  @param sequence_number        The current sequence number used to encode or decode message.
+  @param sequence_number_buffer  A buffer to hold the sequence number output used in the secured message.
+                               The size in byte of the output buffer shall be 8.
 
   @return size in byte of the sequence_number_buffer.
           It shall be no greater than 8.
           0 means no sequence number is required.
 **/
 uint8_t spdm_none_get_sequence_number(uint64_t sequence_number,
-                                      uint8_t* sequence_number_buffer)
+                    uint8_t *sequence_number_buffer)
 {
     return 0;
 }
