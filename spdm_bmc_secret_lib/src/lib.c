bd910f16a8c9a92da3b4e7acb7edb32736d3d992
Prithvi A Pai <prithvi.a.pai@intel.com>
2024-04-15 16:37:06 +0530

    spdmapplib: Remove function implementation (#90)
    Added patch to remove some function implementation. The implementation
    is reference provided by opensource library. For our use case we dont
    need the implementation as it is NOT the POR.
    
    Tested:
    1) BMC builds fine
    2) BMC as SPDM Responder works fine
    
    Signed-off-by: Prithvi A Pai <prithvi.a.pai@intel.com>

--- spdm_bmc_secret_lib/src/lib.c
+++ spdm_bmc_secret_lib/src/lib.c
@@ -102,137 +102,20 @@ bool libspdm_read_responder_private_key(uint32_t base_asym_algo,
 bool libspdm_read_requester_private_key(uint16_t req_base_asym_alg,
                                         void **data, size_t *size)
 {
-    bool res;
-    char *file;
-
-    switch (req_base_asym_alg) {
-    case SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_RSASSA_2048:
-    case SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_RSAPSS_2048:
-        file = "rsa2048/end_requester.key";
-        break;
-    case SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_RSASSA_3072:
-    case SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_RSAPSS_3072:
-        file = "rsa3072/end_requester.key";
-        break;
-    case SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_RSASSA_4096:
-    case SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_RSAPSS_4096:
-        file = "rsa4096/end_requester.key";
-        break;
-    case SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_ECDSA_ECC_NIST_P256:
-        file = "ecp256/end_requester.key";
-        break;
-    case SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_ECDSA_ECC_NIST_P384:
-        file = "ecp384/end_requester.key";
-        break;
-    case SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_ECDSA_ECC_NIST_P521:
-        file = "ecp521/end_requester.key";
-        break;
-    case SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_SM2_ECC_SM2_P256:
-        file = "sm2/end_requester.key";
-        break;
-    case SPDM_ALGORITHMS_BASE_ASYM_ALGO_EDDSA_ED25519:
-        file = "ed25519/end_requester.key";
-        break;
-    case SPDM_ALGORITHMS_BASE_ASYM_ALGO_EDDSA_ED448:
-        file = "ed448/end_requester.key";
-        break;
-    default:
-        LIBSPDM_ASSERT(false);
-        return false;
-    }
-    res = libspdm_read_input_file(file, data, size);
-    return res;
+    return false;
 }
 #endif /* LIBSPDM_ENABLE_CAPABILITY_MUT_AUTH_CAP */
 
 bool libspdm_read_responder_public_key(uint32_t base_asym_algo,
                                        void **data, size_t *size)
 {
-    bool res;
-    char *file;
-
-    switch (base_asym_algo) {
-    case SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_RSASSA_2048:
-    case SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_RSAPSS_2048:
-        file = "rsa2048/end_responder.key.pub.der";
-        break;
-    case SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_RSASSA_3072:
-    case SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_RSAPSS_3072:
-        file = "rsa3072/end_responder.key.pub.der";
-        break;
-    case SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_RSASSA_4096:
-    case SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_RSAPSS_4096:
-        file = "rsa4096/end_responder.key.pub.der";
-        break;
-    case SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_ECDSA_ECC_NIST_P256:
-        file = "ecp256/end_responder.key.pub.der";
-        break;
-    case SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_ECDSA_ECC_NIST_P384:
-        file = "ecp384/end_responder.key.pub.der";
-        break;
-    case SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_ECDSA_ECC_NIST_P521:
-        file = "ecp521/end_responder.key.pub.der";
-        break;
-    case SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_SM2_ECC_SM2_P256:
-        file = "sm2/end_responder.key.pub.der";
-        break;
-    case SPDM_ALGORITHMS_BASE_ASYM_ALGO_EDDSA_ED25519:
-        file = "ed25519/end_responder.key.pub.der";
-        break;
-    case SPDM_ALGORITHMS_BASE_ASYM_ALGO_EDDSA_ED448:
-        file = "ed448/end_responder.key.pub.der";
-        break;
-    default:
-        LIBSPDM_ASSERT(false);
-        return false;
-    }
-    res = libspdm_read_input_file(file, data, size);
-    return res;
+    return false;
 }
 
 bool libspdm_read_requester_public_key(uint16_t req_base_asym_alg,
                                        void **data, size_t *size)
 {
-    bool res;
-    char *file;
-
-    switch (req_base_asym_alg) {
-    case SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_RSASSA_2048:
-    case SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_RSAPSS_2048:
-        file = "rsa2048/end_requester.key.pub.der";
-        break;
-    case SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_RSASSA_3072:
-    case SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_RSAPSS_3072:
-        file = "rsa3072/end_requester.key.pub.der";
-        break;
-    case SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_RSASSA_4096:
-    case SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_RSAPSS_4096:
-        file = "rsa4096/end_requester.key.pub.der";
-        break;
-    case SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_ECDSA_ECC_NIST_P256:
-        file = "ecp256/end_requester.key.pub.der";
-        break;
-    case SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_ECDSA_ECC_NIST_P384:
-        file = "ecp384/end_requester.key.pub.der";
-        break;
-    case SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_ECDSA_ECC_NIST_P521:
-        file = "ecp521/end_requester.key.pub.der";
-        break;
-    case SPDM_ALGORITHMS_BASE_ASYM_ALGO_TPM_ALG_SM2_ECC_SM2_P256:
-        file = "sm2/end_requester.key.pub.der";
-        break;
-    case SPDM_ALGORITHMS_BASE_ASYM_ALGO_EDDSA_ED25519:
-        file = "ed25519/end_requester.key.pub.der";
-        break;
-    case SPDM_ALGORITHMS_BASE_ASYM_ALGO_EDDSA_ED448:
-        file = "ed448/end_requester.key.pub.der";
-        break;
-    default:
-        LIBSPDM_ASSERT(false);
-        return false;
-    }
-    res = libspdm_read_input_file(file, data, size);
-    return res;
+    return false;
 }
 
 #if LIBSPDM_ENABLE_CAPABILITY_CSR_CAP
@@ -262,101 +145,7 @@ bool libspdm_gen_csr(uint32_t base_hash_algo, uint32_t base_asym_algo, bool *nee
                      size_t *csr_len, uint8_t *csr_pointer,
                      bool is_device_cert_model)
 {
-    bool result;
-    size_t hash_nid;
-    size_t asym_nid;
-    void *context;
-
-    uint8_t *cached_last_csr_request;
-    size_t cached_last_request_len;
-    uint8_t *cached_csr;
-    size_t csr_buffer_size;
-
-    csr_buffer_size = *csr_len;
-
-    /*device gen csr need reset*/
-    if (*need_reset) {
-        result = libspdm_read_cached_last_csr_request(&cached_last_csr_request,
-                                                      &cached_last_request_len);
-
-        /*get the cached last csr request and csr*/
-        if ((result) &&
-            (cached_last_request_len == request_size) &&
-            (libspdm_consttime_is_mem_equal(cached_last_csr_request, request,
-                                            request_size)) &&
-            (libspdm_read_cached_csr(&cached_csr, csr_len)) &&
-            (*csr_len != 0)) {
-
-            /*get and save cached csr*/
-            if (csr_buffer_size < *csr_len) {
-                free(cached_csr);
-                free(cached_last_csr_request);
-                LIBSPDM_DEBUG((LIBSPDM_DEBUG_INFO,
-                               "csr buffer is too small to sotre cached csr! \n"));
-                return false;
-            } else {
-                libspdm_copy_mem(csr_pointer, csr_buffer_size, cached_csr, *csr_len);
-            }
-
-            /*device don't need reset this time*/
-            *need_reset = false;
-
-            free(cached_csr);
-            free(cached_last_csr_request);
-            return true;
-        } else {
-            if (cached_last_csr_request != NULL) {
-                free(cached_last_csr_request);
-            }
-
-            /*device need reset this time: cache the last_csr_request */
-            result = libspdm_cache_last_csr_request(request, request_size);
-            if (!result) {
-                return result;
-            }
-
-            /*device need reset this time*/
-            *need_reset = true;
-            return true;
-        }
-    }
-    if (g_private_key_mode) {
-        void *prikey;
-        size_t prikey_size;
-
-        result = libspdm_read_responder_private_key(
-            base_asym_algo, &prikey, &prikey_size);
-        if (!result) {
-            return false;
-        }
-
-        result = libspdm_asym_get_private_key_from_pem(
-            base_asym_algo, prikey, prikey_size, NULL, &context);
-        if (!result) {
-            libspdm_zero_mem(prikey, prikey_size);
-            free(prikey);
-            return false;
-        }
-        hash_nid = libspdm_get_hash_nid(base_hash_algo);
-        asym_nid = libspdm_get_aysm_nid(base_asym_algo);
-
-        char *subject_name = "C=NL,O=PolarSSL,CN=PolarSSL Server 1";
-
-        result = libspdm_gen_x509_csr(hash_nid, asym_nid,
-                                      requester_info, requester_info_length,
-                                      !is_device_cert_model,
-                                      context, subject_name,
-                                      csr_len, csr_pointer);
-        libspdm_asym_free(base_asym_algo, context);
-        libspdm_zero_mem(prikey, prikey_size);
-        free(prikey);
-    }
-
-    if (csr_buffer_size < *csr_len) {
-        LIBSPDM_DEBUG((LIBSPDM_DEBUG_INFO,"csr buffer is too small to sotre generated csr! \n"));
-        result = false;
-    }
-    return result;
+    return false;
 }
 #endif /* LIBSPDM_ENABLE_CAPABILITY_CSR_CAP */
 
@@ -632,18 +421,7 @@ bool libspdm_encap_challenge_opaque_data(
     void *opaque_data,
     size_t *opaque_data_size)
 {
-    size_t index;
-
-    LIBSPDM_ASSERT(libspdm_secret_lib_challenge_opaque_data_size <= *opaque_data_size);
-
-    *opaque_data_size = libspdm_secret_lib_challenge_opaque_data_size;
-
-    for (index = 0; index < *opaque_data_size; index++)
-    {
-        ((uint8_t *)opaque_data)[index] = (uint8_t)index;
-    }
-
-    return true;
+    return false;
 }
 
 bool libspdm_generate_measurement_summary_hash(
@@ -788,44 +566,7 @@ bool libspdm_requester_data_sign(
     const uint8_t *message, size_t message_size,
     uint8_t *signature, size_t *sig_size)
 {
-    void *context = NULL;
-    bool result = false;
-
-    if (g_private_key_mode) {
-        void *private_pem;
-        size_t private_pem_size;
-
-        result = libspdm_read_requester_private_key(
-            req_base_asym_alg, &private_pem, &private_pem_size);
-        if (!result) {
-            return false;
-        }
-
-        result = libspdm_req_asym_get_private_key_from_pem(req_base_asym_alg,
-                                                           private_pem,
-                                                           private_pem_size, NULL,
-                                                           &context);
-        if (!result) {
-            libspdm_zero_mem(private_pem, private_pem_size);
-            free(private_pem);
-            return false;
-        }
-
-        if (is_data_hash) {
-            result = libspdm_req_asym_sign_hash(spdm_version, op_code, req_base_asym_alg,
-                                                base_hash_algo, context,
-                                                message, message_size, signature, sig_size);
-        } else {
-            result = libspdm_req_asym_sign(spdm_version, op_code, req_base_asym_alg,
-                                           base_hash_algo, context,
-                                           message, message_size,
-                                           signature, sig_size);
-        }
-        libspdm_req_asym_free(req_base_asym_alg, context);
-        libspdm_zero_mem(private_pem, private_pem_size);
-        free(private_pem);
-    }
-    return result;
+    return false;
 }
 #endif /* LIBSPDM_ENABLE_CAPABILITY_MUT_AUTH_CAP */
 
@@ -875,16 +616,6 @@ bool libspdm_responder_data_sign(
 }
 
 #if LIBSPDM_ENABLE_CAPABILITY_PSK_CAP
-
-uint8_t m_libspdm_my_zero_filled_buffer[LIBSPDM_MAX_HASH_SIZE];
-uint8_t m_libspdm_bin_str0[0x11] = {
-    0x00, 0x00, /* length - to be filled*/
-    /* SPDM_VERSION_1_1_BIN_CONCAT_LABEL */
-    0x73, 0x70, 0x64, 0x6d, 0x31, 0x2e, 0x31, 0x20,
-    /* SPDM_BIN_STR_0_LABEL */
-    0x64, 0x65, 0x72, 0x69, 0x76, 0x65, 0x64,
-};
-
 bool libspdm_psk_handshake_secret_hkdf_expand(
     spdm_version_number_t spdm_version,
     uint32_t base_hash_algo,
@@ -894,40 +625,7 @@ bool libspdm_psk_handshake_secret_hkdf_expand(
     size_t info_size,
     uint8_t *out, size_t out_size)
 {
-    void *psk;
-    size_t psk_size;
-    size_t hash_size;
-    bool result;
-    uint8_t handshake_secret[LIBSPDM_MAX_HASH_SIZE];
-
-    if (psk_hint_size == 0) {
-        psk = LIBSPDM_TEST_PSK_DATA_STRING;
-        psk_size = sizeof(LIBSPDM_TEST_PSK_DATA_STRING);
-    } else if ((strcmp((const char *)psk_hint, LIBSPDM_TEST_PSK_HINT_STRING) ==
-                0) &&
-               (psk_hint_size == sizeof(LIBSPDM_TEST_PSK_HINT_STRING))) {
-        psk = LIBSPDM_TEST_PSK_DATA_STRING;
-        psk_size = sizeof(LIBSPDM_TEST_PSK_DATA_STRING);
-    } else {
-        return false;
-    }
-    printf("[PSK]: ");
-    libspdm_dump_hex_str(psk, psk_size);
-    printf("\n");
-
-    hash_size = libspdm_get_hash_size(base_hash_algo);
-
-    result = libspdm_hkdf_extract(base_hash_algo, psk, psk_size, m_libspdm_my_zero_filled_buffer,
-                                  hash_size, handshake_secret, hash_size);
-    if (!result) {
-        return result;
-    }
-
-    result = libspdm_hkdf_expand(base_hash_algo, handshake_secret, hash_size,
-                                 info, info_size, out, out_size);
-    libspdm_zero_mem(handshake_secret, hash_size);
-
-    return result;
+    return false;
 }
 
 bool libspdm_psk_master_secret_hkdf_expand(
@@ -939,58 +637,7 @@ bool libspdm_psk_master_secret_hkdf_expand(
     size_t info_size, uint8_t *out,
     size_t out_size)
 {
-    void *psk;
-    size_t psk_size;
-    size_t hash_size;
-    bool result;
-    uint8_t handshake_secret[LIBSPDM_MAX_HASH_SIZE];
-    uint8_t salt1[LIBSPDM_MAX_HASH_SIZE];
-    uint8_t master_secret[LIBSPDM_MAX_HASH_SIZE];
-
-    if (psk_hint_size == 0) {
-        psk = LIBSPDM_TEST_PSK_DATA_STRING;
-        psk_size = sizeof(LIBSPDM_TEST_PSK_DATA_STRING);
-    } else if ((strcmp((const char *)psk_hint, LIBSPDM_TEST_PSK_HINT_STRING) ==
-                0) &&
-               (psk_hint_size == sizeof(LIBSPDM_TEST_PSK_HINT_STRING))) {
-        psk = LIBSPDM_TEST_PSK_DATA_STRING;
-        psk_size = sizeof(LIBSPDM_TEST_PSK_DATA_STRING);
-    } else {
-        return false;
-    }
-
-    hash_size = libspdm_get_hash_size(base_hash_algo);
-
-    result = libspdm_hkdf_extract(base_hash_algo, psk, psk_size, m_libspdm_my_zero_filled_buffer,
-                                  hash_size, handshake_secret, hash_size);
-    if (!result) {
-        return result;
-    }
-
-    *(uint16_t *)m_libspdm_bin_str0 = (uint16_t)hash_size;
-    /* patch the version*/
-    m_libspdm_bin_str0[6] = (char)('0' + ((spdm_version >> 12) & 0xF));
-    m_libspdm_bin_str0[8] = (char)('0' + ((spdm_version >> 8) & 0xF));
-    result = libspdm_hkdf_expand(base_hash_algo, handshake_secret, hash_size,
-                                 m_libspdm_bin_str0, sizeof(m_libspdm_bin_str0), salt1,
-                                 hash_size);
-    libspdm_zero_mem(handshake_secret, hash_size);
-    if (!result) {
-        return result;
-    }
-
-    result = libspdm_hkdf_extract(base_hash_algo, m_libspdm_my_zero_filled_buffer,
-                                  hash_size, salt1, hash_size, master_secret, hash_size);
-    libspdm_zero_mem(salt1, hash_size);
-    if (!result) {
-        return result;
-    }
-
-    result = libspdm_hkdf_expand(base_hash_algo, master_secret, hash_size,
-                                 info, info_size, out, out_size);
-    libspdm_zero_mem(master_secret, hash_size);
-
-    return result;
+    return false;
 }
 #endif /* LIBSPDM_ENABLE_CAPABILITY_PSK_CAP */
 
